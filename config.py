cc896813995af4bcac518de67602590612e3b5f3
LordSan9 <104519846+LordSan9@users.noreply.github.com>
2022-07-23 06:47:44 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+from os import getenv
+
+que = {}
+SESSION_NAME = "AgCCL08cqYZyJtgnrLRFZ1eLCk--16YNIzEa60ZOwtSBrr9WQJssfsylEc7NMbhQAiNWH8qjMf8isXWamQ9DECM2kiugOSK1Sn1-rTmS53tSqthanEO3aHR_TLqU_xQMlnlfESf0nrqHdIP64yICOpWfVqvc6aeHzqXSnBqVKUWZMdThq9Nj71tG8rG-m49-U3XuQP--pgkljLqSJQzYL4_ZOxxeknoeP6K3pCatJB9PA-9ZoHxOHGyiH37pLI8Eu1zXfawnHKeHlyjv23Fv_5-jbIw9o3zmrXuRtWOBY1Vm2hZZYD84AKmaOj-RZIFNtxf00e6ryfe7JNourxuLviKLAAAAAUUUi-cA"
+BOT_TOKEN = "5349622998:AAESuWEpi-NX4Sax_arYqzV1oMsov3bkXtg"
+BOT_NAME = "uDq_bot"
+UPDATES_CHANNEL = "oyurl"
+BG_IMAGE = "https://telegra.ph/file/f0ebe2f627c2867a0a921.jpg"
+admins = {}
+API_ID = "19141537"
+API_HASH = "c9fcc58ebf42803242c664c5b9c3f8c8"
+BOT_USERNAME = "uDq_bot"
+ASSISTANT_NAME = "IlBoSSll"
+SUPPORT_GROUP = "IIlAndylII"
+PROJECT_NAME = "iilllllleeeeeo"
+SOURCE_CODE = getenv("SOURCE_CODE", "github.com/TeamDaisyX/DaisyXMusic")
+DURATION_LIMIT = "300"
+PMPERMIT = getenv("PMPERMIT", None)
+LOG_GRP = getenv("LOG_GRP", None)
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
+SUDO_USERS = "5336448904"
